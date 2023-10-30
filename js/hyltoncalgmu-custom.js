07f2c2041d3a7b9df8809751d99f00efa09de527
Jan Macario <jmacario@gmu.edu>
2023-10-30 14:23:56 -0400

    Changes how the event details button labels for vererans and the arts events are customized in JS, making use of a new CSS class value that is available which indicates the event group.

--- js/hyltoncalgmu-custom.js
+++ js/hyltoncalgmu-custom.js
@@ -8,32 +8,12 @@ jQuery(document).ready(function(){
 	// Fix href of site logo to go back to main Hylton site
 	jQuery(".site-logo a").prop("href", "https://hyltoncenter.org")
 
-	// Fix text of event details buttons for the veterans and the arts initiative events
-	jQuery('.event-entry-veterans-and-the-arts-initiative-summer-2022-veterans-virtual-guitar-workshops-level-i .event_details_button a').text('register');
-	jQuery('.event-entry-veterans-and-the-arts-initiative-summer-2022-veterans-virtual-guitar-workshops-level-ii .event_details_button a').text('register');
-	jQuery('.event-entry-veterans-virtual-guitar-workshops-level-i .event_details_button a').text('register');
-	jQuery('.event-entry-veterans-virtual-guitar-workshops-level-ii .event_details_button a').text('register');
-	jQuery('.event-entry-basic-landscape-photography-using-a-smartphone .event_details_button a').text('register');
-	jQuery('.event-entry-sneaker-sculpture-workshops .event_details_button a').text('register');
-	jQuery('.event-entry-basic-portrait-photography-using-a-smartphone .event_details_button a').text('register');
-	jQuery('.event-entry-basic-close-up-and-macro-photography-using-a-smartphone .event_details_button a').text('register');
-	jQuery('.event-entry-exploring-innovative-collage-techniques-through-botanical-imagery .event_details_button a').text('register');
+	// Fix text of event details buttons for all the veterans and the arts initiative events
+	jQuery('.event-entry.event-group-veterans-and-the-arts-initiative .event_details_button a').text('Register');
 
-	jQuery('.event-entry-spring-2023-veterans-virtual-guitar-workshops-level-i .event_details_button a').text('register');
-	jQuery('.event-entry-spring-2023-veterans-virtual-guitar-workshops-level-ii .event_details_button a').text('register');
-	jQuery('.event-entry-spring-2023-songwriting-series-for-military-kids .event_details_button a').text('register');
-	jQuery('.event-entry-graphic-arts-8211-exploration-of-hands-on-arts-practices .event_details_button a').text('register');
-	jQuery('.event-entry-spring-2023-ukulele-workshop-series-level-i .event_details_button a').text('register');
-	jQuery('.event-entry-spring-2023-ukulele-workshop-series-level-ii .event_details_button a').text('register');
-	jQuery('.event-entry-observing-the-world-through-a-lens-photography-workshop-series-for-women-veterans .event_details_button a').text('register');
-	jQuery('.event-entry-spring-2023-basic-landscape-photography-using-a-smartphone .event_details_button a').text('register');
-
-	jQuery('.event-entry-ukulele-workshop-hum-and-strum-at-central-library .event_details_button a').text('Register');
-	jQuery('.event-entry-ukulele-workshop-hum-and-strum-at-potomac-library .event_details_button a').text('Register');
-	jQuery('.event-entry-eco-graphic-arts-workshops .event_details_button a').text('Register');
-	jQuery('.event-entry-veterans-virtual-guitar-workshops-level-i .event_details_button a').text('Register');
-	jQuery('.event-entry-veterans-virtual-guitar-workshops-level-ii .event_details_button a').text('Register');
-	jQuery('.event-entry-improv-for-military-kids .event_details_button a').text('Register');
+	// ...but not for some specific events
+	jQuery('.event-entry-the-u-s-army-blues-veterans-tribute .event_details_button a').text('Get Tickets');
+	jQuery('.event-entry-the-u-s-army-band-star-spangled-spectacular-with-christopher-jackson .event_details_button a').text('Get Tickets');
 
 	// Fix text of event details buttons for the gallery exhibitions
 	jQuery('.event-entry-gallery-exhibit-bennie-herron-zero-and-one .event_details_button a').text('Learn More');
